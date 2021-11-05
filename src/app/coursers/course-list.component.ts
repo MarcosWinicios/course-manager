870945177be965074824ab73363ece6573f5bcd2
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-04 21:41:54 -0300

    created search with filter

--- src/app/coursers/course-list.component.ts
+++ src/app/coursers/course-list.component.ts
@@ -1,17 +1,30 @@
 import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { TranslationWidth } from '@angular/common';
 
 @Component({
     selector: "app-course-list",
     templateUrl: "./course-list.component.html"
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
-
+    filteredCourses: Course[] = [];
+    _courses: Course[] = [];
+    _filterBy: string;
     constructor(private couseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courses = this.couseService.retriveAll();
+        this._courses = this.couseService.retriveAll();
+        this.filteredCourses = this._courses;
+    }
+    set filter(value: string) {
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course
+            .name
+            .toLocaleLowerCase()
+            .indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+    get filter() {
+        return this._filterBy;
     }
 }
