4adcbed36448c65668cda482e737598f5bff6287
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-07 17:08:37 -0300

    separando components pesados na pasta core

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,23 +1,23 @@
+import { CoreModule } from './core/core.module';
 import { CourseModule } from './coursers/course.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppComponent } from './app.component';
 import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: "", redirectTo: "courses", pathMatch: "full" //Raiz do projeto
