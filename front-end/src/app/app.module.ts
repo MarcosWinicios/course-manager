f9b1e3a94d36b8c0e6a14aafe7bf9877c02f55ad
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-07 17:01:44 -0300

    criando components globais com shared

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { CourseModule } from './coursers/course.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
 import { AppComponent } from './app.component';
 import { Error404Component } from './error-404/error-404.component';
 
--- front-end/src/app/coursers/course.module.ts
+++ front-end/src/app/coursers/course.module.ts
@@ -1,21 +1,21 @@
-import { ReplacePipe } from './../pipe/replace.pipe';
+import { AppPipeModule } from './../shared/pipes/app-pipe.module';
+import { StarModule } from './../shared/components/star/star.module';
 import { CourseInfoComponent } from './course-info.component';
 import { CourseListComponent } from './course-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from '../star/star.component';
 @NgModule({
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
     ],
     imports: [
         CommonModule,
         FormsModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: "courses", component: CourseListComponent //Rota de algum componente específico
